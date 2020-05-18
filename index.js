7626e886dc2ef8c7146811e24f5c3725bdb04105
Yilmaz <ext-bilge.yilmaz@here.com>
2020-05-19 00:29:02 +0200

    Exercises 3.15.-3.18.

--- index.js
+++ index.js
@@ -30,12 +30,17 @@ app.get('/', (req, res) => {
   res.send('<h1>Phone Book!</h1>')
 })
 
-app.get('/info', (req, res) => {
-    const len = persons.length
+app.get('/info', (request, response) => {
+    let len = 0;
     const date = new Date()
-    res.send(`
+
+    Person.find({}).then(persons => {
+      len = persons.length
+      response.send(`
             <p>Phonebook has info for ${len} people</p>
             <p>${date}</p>`)
+    })
+
   })
   
 
@@ -45,13 +50,18 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
+  console.log(request.params.id)
 
-
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
-
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end() 
+      }
+    })
+    .catch(error => next(error))
 })
 
 
@@ -74,11 +84,41 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
